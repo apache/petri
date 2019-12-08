9b30a5a71b2a09ee2e47baa1a1ebbebc76a5184c
Greg Stein <gstein@gmail.com>
2019-12-08 09:03:35 -0600

    Add a simple bootstrap-based theme.
    * use bootstrap, jquery, popper, and github-markdown-css
    * add a draft top-navbar, and footer/copyright, based on the branding
      guidelines for apache sites
    * disable most skeleton pages: we don't use tags, categories, author
      lists, a managed index, articles, or archives
    * disable paths on the pages; stick to flat
    * apache.css is loaded with recommendations from github-markdown-css
    * use the feather for favicon and navbar icon, until petri decides on
      its own logo

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,20 +20,26 @@ DEFAULT_LANG = u'en'
 SITEURL = 'https://petri.apache.org'
 
 # Save pages using full directory preservation
-PATH_METADATA= '(?P<path_no_ext>.*)\..*'
-PAGE_SAVE_AS= '{path_no_ext}.html'
-PAGE_URL= '{path_no_ext}.html'
+PAGES_PATHS = [ 'content' ]
+#PATH_METADATA= '(?P<path_no_ext>.*)\..*'
+#PAGE_SAVE_AS= '{path_no_ext}.html'
+#PAGE_URL= '{path_no_ext}.html'
+SLUGIFY_SOURCE = 'basename'
+PAGE_SAVE_AS = '{slug}.html'
 
-# Standard behavior:
-#PAGE_SAVE_AS = './{slug}.html'
+# We don't use articles, but we don't want pelican to think
+# that content/ contains articles.
+ARTICLE_PATHS = [ 'articles' ]
 
-ARTICLE_SAVE_AS = 'news/{slug}.html'
-ARTICLE_URL = 'news/{slug}.html'
+# Disable these pages
+ARCHIVES_SAVE_AS = ''
+ARTICLE_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+INDEX_SAVE_AS = ''
+TAGS_SAVE_AS = ''
 
-# Sort news by date, descending, latest article first
-ARTICLE_ORDER_BY = 'reversed-date'
-
-# Feed generation is usually not desired when developing
+# Disable feeds
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -42,7 +48,7 @@ AUTHOR_FEED_RSS = None
 
 # TOC Generator
 PLUGIN_PATHS = ['./theme/plugins']
-PLUGINS = ['toc']
+#PLUGINS = ['toc']
 TOC_HEADERS = r"h[1-6]"
 
 # Unused links
