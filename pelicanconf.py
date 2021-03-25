38f1e5f8f7eb221383f016ff9f0bfa220ca07b8a
Dave Fisher <dave2wave@comcast.net>
2021-03-25 09:58:58 -0700

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -69,7 +69,7 @@ AUTHOR_FEED_RSS = None
 
 # Pelican Plugins
 # The provided location. If the buildbot does not have a new plugin then look into requirements.txt
-PLUGIN_PATHS = ['./plugins']
+# PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['toc', 'pelican-gfm', 'sitemap']
 
 # TOC Generator
